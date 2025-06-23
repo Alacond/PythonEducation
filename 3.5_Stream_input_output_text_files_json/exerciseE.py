f557f6b55f75cb26288a6d269a56dc02b86d2537
Evgeniy Iurchenko <e.yurchenko@cubicgames.com>
2025-06-23 17:40:43 +0400

    last ex more beautiful solution

--- 3.5_Stream_input_output_text_files_json/exerciseE.py
+++ 3.5_Stream_input_output_text_files_json/exerciseE.py
@@ -2,6 +2,35 @@ from sys import stdin
 
 user_string_list = stdin.read().split()
 
+# Множество палиндромов
+palindromes = set()
+
+# Заполняем множество палиндромами. Разные написания слов - будут разными элементами. Так захотел Яндекс
 for word in user_string_list:
-    if word.lower() == word.lower()[::-1]:
-        print(word)
+    lower_word = word.lower()
+    if lower_word == lower_word[::-1]:
+        palindromes.add(word)
+
+# Вывод в алфавитном порядке
+for word in sorted(palindromes):
+    print(word)
+
+# Ниже я привел другое решение, которое избавляется от повторяющихся слов в разном регистре
+
+# from sys import stdin
+
+# user_string_list = stdin.read().split()
+
+# # Словарь палиндромов. Ключ - слово в нижнем регистре, значение - слово как оно встретилось в первый раз
+# palindromes = dict()
+
+# for word in user_string_list:
+#     lower_word = word.lower()
+#     if lower_word == lower_word[::-1]:
+#         # Сохраняем только первое встретившееся представление
+#         if lower_word not in palindromes:
+#             palindromes[lower_word] = word
+
+# # Вывод в алфавитном порядке
+# for word in sorted(palindromes.values()):
+#     print(word)

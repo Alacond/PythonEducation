a49e79004e90c9932028ab6e2a051ea02d9e041f
Evgeniy Iurchenko <e.yurchenko@cubicgames.com>
2025-08-05 11:03:51 +0400

    ex done block 3.5

--- 3.5_Stream_input_output_text_files_json/exerciseO.py
+++ 3.5_Stream_input_output_text_files_json/exerciseO.py
@@ -1,7 +1,23 @@
 import json
 from sys import stdin
 
-with open("D:/PythonEducation/3.5_Stream_input_output_text_files_json/scoring.json", encoding="UTF-8") as scoring_file:
+with open("scoring.json", encoding="UTF-8") as scoring_file:
     score_dict = json.load(scoring_file)
 
-print(score_dict)
+answers = (line.strip() for line in stdin)
+
+score = 0
+
+for group in score_dict:
+    tests = group["tests"]
+    points = group["points"]
+    correct = 0
+
+    for test in tests:
+        user_answer = next(answers)
+        if user_answer == test["pattern"]:
+            correct += 1
+
+    score += correct * points // len(tests)
+
+print(score)
